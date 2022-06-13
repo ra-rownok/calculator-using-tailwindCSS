8dfc32addce3a56f4d6e6d3ce6795bd1f8b4215f
ra-rownok <reshadahamedr001@gmail.com>
2022-06-14 03:57:39 +0600

    code refactor

--- base.js
+++ base.js
@@ -1,33 +1,27 @@
 function setValue(inputID) {
-    document.getElementById(inputID).addEventListener("click",function(){
-        const btnText = document.getElementById(inputID).value;
-        const equal = document.getElementById("b=").value;
+    document.getElementById(inputID).addEventListener("click",function(event){
+        const btnText = event.target.value
         let total = document.getElementById("total");
-        if (btnText != equal){
+        if(total.value == '' && btnText == '+'){
+            btnText.disabled = true;
+        }
+        else if(total.value == '' && btnText == '*'){
+            btnText.disabled = true;
+        }
+        else if(total.value == '' && btnText == '/'){
+            btnText.disabled = true;
+        }
+        else if(total.value == '' && btnText == '='){
+            btnText.disabled = true;
+        }
+        else if (btnText != '='){
             total.value = total.value+ btnText;
         }
-        else {
+        else{
             total.value = eval(total.value)
         }
     })
 }
 
-setValue("b7")
-setValue("b8")
-setValue("b9")
-setValue("b+")
-setValue("b4")
-setValue("b5")
-setValue("b6")
-setValue("b-")
-setValue("b1")
-setValue("b2")
-setValue("b3")
-setValue("b*")
-setValue("b0")
-setValue("b.")
-setValue("b/")
-setValue("b=")
-
-
+setValue("button-text");
 
